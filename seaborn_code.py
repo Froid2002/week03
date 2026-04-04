d3fceb8ae693f71634842c2e185b687828c725e0
Froid2002 <everestfroyd@gmail.com>
2026-04-05 01:26:50 +0900

    Updated Changes

--- seaborn_code.py
+++ seaborn_code.py
@@ -5,4 +5,6 @@ import matplotlib.pyplot as plt
 import os
 
 #create week04 directory if it doesn´t exist to avoid errors
-if not os.path.exists("./week04"): os.makedirs
+if not os.path.exists("./week04"): os.makedirs("./week04")
+
+#
